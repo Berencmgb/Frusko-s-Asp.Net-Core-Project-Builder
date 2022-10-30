f46639d3f78f1a52ad0c0fef3713c3d90d3eea03
Beren Coates-Mullins <BerenCMGB@gmail.com>
2022-10-30 01:28:11 +0100

    FCACPG-34 Fixed bug with adaptive url not having right namespace. FCACPG-46 Removed swagger from API program.cs file

--- ASP.NET.Core_Project_Builder/Boilerplate/ApiTemplates.cs
+++ ASP.NET.Core_Project_Builder/Boilerplate/ApiTemplates.cs
@@ -32,8 +32,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
 {
--- ASP.NET.Core_Project_Builder/Boilerplate/ServiceClientTemplates.cs
+++ ASP.NET.Core_Project_Builder/Boilerplate/ServiceClientTemplates.cs
@@ -18,6 +18,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using {project}.Domain.Models;
 
 namespace {project}.ServiceClient;
 
